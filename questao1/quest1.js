f53a66d54db752de9021a102b122ba1abf56aa1b
AnaVivi <avsl2003@hotmail.com>
2023-09-08 15:10:10 -0300

    adicionando separadores entre os resultados das  operções

--- questao1/quest1.js
+++ questao1/quest1.js
@@ -6,8 +6,8 @@ document.getElementById('result-calculator').addEventListener('click', function(
     let number2 = parseInt(number);    
     let newElement = document.createElement('p');
     newElement.textContent = 'Soma: '+ (number2 + number2) + "\n" +
-    'Substração: ' + (number2 - number2) + "\n" +
-    'Multiplicação: ' + (number2 * number2) + "\n" +
-    'Divisão: ' + (number2 / number2);
+    '| Substração: ' + (number2 - number2) + "\n" +
+    '| Multiplicação: ' + (number2 * number2) + "\n" +
+    '| Divisão: ' + (number2 / number2);
     NumberEnter.appendChild(newElement);
 });
